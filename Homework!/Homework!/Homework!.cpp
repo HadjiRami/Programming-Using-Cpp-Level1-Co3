43b362d635a1ea1e10ea0e5cb7122d31f65c4e1e
HadjiRami <hadji.rami@tutanota.com>
2025-03-11 12:38:46 -0400

    Homework
    Print Multiple Messages in One Line in Code C++
    CardID & Stars

---
+++ Homework!/Homework!/Homework!.cpp
@@ -0,0 +1,28 @@
+// Homework!.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void PrintCardID() {
+	cout << "*******************\n";
+	cout << "Name :" << "Redacted Name.\n";
+	cout << "Age : " << "44 Years.\n";
+	cout << "city :" << "Tampa\n";
+	cout << "Country: " << "USA\n";
+	cout << "*******************\n";
+
+}
+void PrintStars() {
+	cout << "*************\n";
+	cout << "*************\n";
+	cout << "*************\n";
+	cout << "*************\n";
+	
+}
+
+int main()
+{
+	PrintCardID();
+	PrintStars();
+}
+
